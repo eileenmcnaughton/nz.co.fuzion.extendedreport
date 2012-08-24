de47faf1e0f34979dac2a5262ed0559b81e4e924
eileenmcnaughton <eileen@mcnaughty.com>
2012-08-24 17:08:50 +1200

    Update CRM/Extendedreport/Form/Report/Price/Contributionbased.php
    Add extra columns to contribution based report

--- CRM/Extendedreport/Form/Report/Price/Contributionbased.php
+++ CRM/Extendedreport/Form/Report/Price/Contributionbased.php
@@ -2,24 +2,24 @@
 
 require_once 'CRM/Report/Form.php';
 
-class CRM_Extendedreport_Form_Report_Price_Contributionbased extends CRM_Report_Form_Extendedreport {
+class CRM_Extendedreport_Form_Report_Price_Contributionbased extends CRM_Report_Form {
+  protected $_baseTable = 'civicrm_contribution';
+  function __construct() {
+    $this->_columns = $this->getContactColumns()
+    + $this->getContributionColumns()
+    + $this->getPriceFieldColumns()
+    + $this->getPriceFieldValueColumns()
+    + $this->getLineItemColumns();
 
-    protected $_baseTable = 'civicrm_contribution' ;
 
-    function __construct( ) {
-
-        $this->_columns = $this->getContactColumns()
-                        + $this->getContributionColumns()
-                        + $this->getLineItemColumns()
-
-                        ;
-       parent::__construct( );
-    }
-
-    function fromClauses( ) {
-      return array(
+    parent::__construct();
+  }
+  function fromClauses() {
+    return array(
         'lineItem_from_contribution',
         'contact_from_contribution',
-      );
-    }
+        'priceFieldValue_from_lineItem',
+        'priceField_from_lineItem'
+    );
+  }
 }
