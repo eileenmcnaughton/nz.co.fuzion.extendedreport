42040f4645c7930adaaa6a951d7634563960c6ba
eileen <eileen@mcnaughty.com>
2013-09-18 15:21:26 +1200

    Activity Extended - ass assignee + some tidy ups

--- CRM/Extendedreport/Form/Report/ActivityExtended.php
+++ CRM/Extendedreport/Form/Report/ActivityExtended.php
@@ -47,6 +47,8 @@ class CRM_Extendedreport_Form_Report_ActivityExtended extends CRM_Extendedreport
     $this->_columns = $this->getContactColumns()
     + $this->getContactColumns(array('prefix' => '', 'prefix_label' => 'Source Contact ::', 'filters' => TRUE))
     + $this->getContactColumns(array('prefix' => 'target_', 'prefix_label' => 'Target Contact ::', 'filters' => FALSE))
+    + $this->getContactColumns(array('prefix' => 'assignee_', 'prefix_label' => 'Assignee Contact ::', 'filters' => FALSE))
+
     + $this->getActivityColumns();
 
     parent::__construct();
@@ -62,28 +64,28 @@ class CRM_Extendedreport_Form_Report_ActivityExtended extends CRM_Extendedreport
     $this->joinActivityTargetFromActivity();
     $this->joinActivityAssigneeFromActivity();
     $this->joinActivitySourceFromActivity();
-    $this->_from .= "
-
-   {$this->_aclFrom}
-
-   LEFT JOIN civicrm_case_activity case_activity_civireport
-   ON case_activity_civireport.activity_id = {$this->_aliases['civicrm_activity']}.id
-   LEFT JOIN civicrm_case
-   ON case_activity_civireport.case_id = civicrm_case.id ";
+    $this->_from .= " {$this->_aclFrom} ";
+   if ($this->isTableSelected('civicrm_case')) {
+     $this->_from .= "
+       LEFT JOIN civicrm_case_activity case_activity_civireport
+         ON case_activity_civireport.activity_id = {$this->_aliases['civicrm_activity']}.id
+       LEFT JOIN civicrm_case
+         ON case_activity_civireport.case_id = civicrm_case.id ";
+    }
 
    if ($this->isTableSelected('civicrm_email')) {
-   $this->_from .= "
-   LEFT JOIN civicrm_email civicrm_email_source
-   ON {$this->_aliases['civicrm_activity']}.source_contact_id = civicrm_email_source.contact_id AND
-   civicrm_email_source.is_primary = 1
+     $this->_from .= "
+       LEFT JOIN civicrm_email civicrm_email_source
+         ON {$this->_aliases['civicrm_activity']}.source_contact_id = civicrm_email_source.contact_id
+         AND civicrm_email_source.is_primary = 1
 
-   LEFT JOIN civicrm_email civicrm_email_target
-                         ON {$this->_aliases['civicrm_activity_target']}.target_contact_id = civicrm_email_target.contact_id AND
-                         civicrm_email_target.is_primary = 1
+       LEFT JOIN civicrm_email civicrm_email_target
+         ON {$this->_aliases['civicrm_activity_target']}.target_contact_id = civicrm_email_target.contact_id
+         AND civicrm_email_target.is_primary = 1
 
-                         LEFT JOIN civicrm_email civicrm_email_assignee
-                         ON {$this->_aliases['civicrm_activity_assignment']}.assignee_contact_id = civicrm_email_assignee.contact_id AND
-                         civicrm_email_assignee.is_primary = 1 ";
+       LEFT JOIN civicrm_email civicrm_email_assignee
+        ON {$this->_aliases['civicrm_activity_assignment']}.assignee_contact_id = civicrm_email_assignee.contact_id
+        AND civicrm_email_assignee.is_primary = 1 ";
     }
     $this->addAddressFromClause();
     $this->selectableCustomDataFrom();
@@ -93,17 +95,9 @@ class CRM_Extendedreport_Form_Report_ActivityExtended extends CRM_Extendedreport
    *
    */
   function postProcess() {
-
-    $this->beginPostProcess();
-
     // get the acl clauses built before we assemble the query
+    //@todo - find out why the parent doesn't do this - or if it now does
     $this->buildACLClause($this->_aliases['civicrm_contact']);
-    $sql = $this->buildQuery(TRUE);
-    $rows = array();
-    $this->buildRows($sql, $rows);
-
-    $this->formatDisplay($rows);
-    $this->doTemplateAssignment($rows);
-    $this->endPostProcess($rows);
+    parent::postProcess();
   }
 }
