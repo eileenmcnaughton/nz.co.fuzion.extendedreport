34c737466ace12d1139ef8f90f56c22d614562a4
Eileen <eileen@fuzion.co.nz>
2013-10-29 23:09:01 -0400

    add handling for people who don't have access civicrm so they don't see non-functional links

--- CRM/Extendedreport/Form/Report/ExtendedReport.php
+++ CRM/Extendedreport/Form/Report/ExtendedReport.php
@@ -4403,6 +4403,16 @@ ON ({$this->_aliases['civicrm_event']}.id = {$this->_aliases['civicrm_participan
    */
   function alterContactID($value, &$row, $fieldname) {
     $nameField = substr($fieldname,0, -2) . 'name';
+    static $first = TRUE;
+    static $viewContactList = FALSE;
+    if($first) {
+      $viewContactList = CRM_Core_Permission::check('access CiviCRM');
+      $first = FALSE;
+    }
+
+    if(!$viewContactList) {
+      return $value;
+    }
     if(array_key_exists($nameField, $row)) {
       $row[$nameField . '_link'] = CRM_Utils_System::url("civicrm/contact/view", 'reset=1&cid=' . $value, $this->_absoluteUrl);
     }
