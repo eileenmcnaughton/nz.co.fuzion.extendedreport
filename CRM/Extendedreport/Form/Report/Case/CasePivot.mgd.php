ae79f5f7a0fcde1ca84ed94d837c5264a99a2d0d
Eileen McNaughton <eileen@fuzion.co.nz>
2014-01-25 21:09:58 +1300

    Case pivot report

---
+++ CRM/Extendedreport/Form/Report/Case/CasePivot.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 =>
+  array (
+    'name' => 'Extended Report - Case Pivot Chart',
+    'entity' => 'ReportTemplate',
+    'params' =>
+    array (
+      'version' => 3,
+      'label' => 'Extended Report - Case Pivot Chart',
+      'description' => 'Extended Report - Case Pivot Chart',
+      'class_name' => 'CRM_Extendedreport_Form_Report_Case_CasePivot',
+      'report_url' => 'case/pivot',
+      'component' => 'CiviCase',
+    ),
+  ),
+);
---
+++ CRM/Extendedreport/Form/Report/Case/CasePivot.php
@@ -0,0 +1,47 @@
+<?php
+
+class CRM_Extendedreport_Form_Report_Case_CasePivot extends CRM_Extendedreport_Form_Report_ExtendedReport {
+  protected $_baseTable = 'civicrm_case';
+  protected $skipACL = true;
+  protected $_customGroupAggregates = true;
+  protected $_aggregatesIncludeNULL = TRUE;
+  protected $_aggregatesAddTotal = TRUE;
+  protected $_rollup = 'WITH ROLLUP';
+  public $_drilldownReport = array();
+  protected $_potentialCriteria = array(
+  );
+
+  function __construct() {
+    $this->_customGroupExtended['civicrm_case'] = array(
+      'extends' => array('Case'),
+      'filters' => TRUE,
+      'title'  => ts('Case'),
+    );
+
+    $this->_columns = $this->getColumns('Case', array(
+      'fields' => false,)
+    )
+    + $this->getColumns('Contact', array());
+    $this->_columns['civicrm_case']['fields']['id']['required'] = true;
+    $this->_columns['civicrm_contact']['fields']['id']['required'] = true;
+ //  $this->_columns['civicrm_case']['fields']['id']['alter_display'] = 'alterCaseID';
+    $this->_columns['civicrm_case']['fields']['id']['title'] = 'Case';
+    $this->_columns['civicrm_contact']['fields']['gender_id']['no_display'] = true;
+    $this->_columns['civicrm_contact']['fields']['gender_id']['title'] = 'Gender';
+
+    $this->_aggregateRowFields  = array(
+      'case_civireport:id' => 'Case',
+      'civicrm_contact_civireport:gender_id' => 'Gender',
+    );
+    $this->_aggregateColumnHeaderFields  = array(
+      'civicrm_contact_civireport:gender_id' => 'Gender',
+    );
+    parent::__construct();
+  }
+
+  function fromClauses( ) {
+    return array(
+      'contact_from_case',
+    );
+  }
+}
