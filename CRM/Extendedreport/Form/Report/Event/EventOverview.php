2bf1c8edd4c75f8184c83cd1a8a5f7c207246645
Eileen McNaughton <eileen@fuzion.co.nz>
2014-01-29 07:29:02 +1300

    Fix e-notice

--- CRM/Extendedreport/Form/Report/Event/EventOverview.php
+++ CRM/Extendedreport/Form/Report/Event/EventOverview.php
@@ -8,7 +8,7 @@ class CRM_Extendedreport_Form_Report_Event_EventOverview extends CRM_Extendedrep
   protected $_autoIncludeIndexedFieldsAsOrderBys = TRUE;
 
   function __construct() {
-    $this->_columns = $this->getEventColumns()
+    $this->_columns = $this->getColumns('Event', array())
     +$this->getEventSummaryColumns();
     parent::__construct();
   }
