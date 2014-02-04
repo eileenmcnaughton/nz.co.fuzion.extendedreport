fc7f522ea3b9020b4937c2d64821d3ad5f79808b
Eileen <eileen@fuzion.co.nz>
2014-02-04 16:36:56 +1300

    Add editable activity report

---
+++ CRM/Extendedreport/Form/Report/ActivityEditable.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 =>
+  array (
+    'name' => 'Extended Report - Editable Activities',
+    'entity' => 'ReportTemplate',
+    'params' =>
+    array (
+      'version' => 3,
+      'label' => 'Extended Report - Editable Activities',
+      'description' => 'Extended Report - editable Activity Report',
+      'class_name' => 'CRM_Extendedreport_Form_Report_ActivityEditable',
+      'report_url' => 'activityeditable',
+      'component' => '',
+    ),
+  ),
+);
---
+++ CRM/Extendedreport/Form/Report/ActivityEditable.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Class CRM_Extendedreport_Form_Report_ActivityExtended
+ */
+class CRM_Extendedreport_Form_Report_ActivityEditable extends CRM_Extendedreport_Form_Report_ExtendedReport {
+//todo move def to getActivityColumns
+  /**
+   * @var array
+   */
+  protected $_customGroupExtends = array('Activity');
+  /**
+   * @var bool
+   */
+  protected $_addressField = FALSE;
+  /**
+   * @var bool
+   */
+  protected $_emailField = FALSE;
+  /**
+   * @var null
+   */
+  protected $_summary = NULL;
+  /**
+   * @var bool
+   */
+  protected $_exposeContactID = FALSE;
+  /**
+   * @var bool
+   */
+  protected $_customGroupGroupBy = FALSE;
+  /**
+   * @var string
+   */
+  protected $_baseTable = 'civicrm_activity';
+
+  /**
+   * constructor
+   * @todo allow filtering on other contacts
+   */
+  function __construct() {
+    $this->_columns = $this->getActivityColumns()
+     + $this->getColumns('Contact', array('fields' => false, 'prefix' => 'target_',));
+   $this->_columns['civicrm_activity']['fields']['id'] = array(
+     'title' => 'id',
+     'required' => TRUE,
+   );
+    parent::__construct();
+  }
+
+  /**
+   * Generate From clause
+   * @todo Should remove all this to parent class
+   */
+  function fromClauses( ) {
+    return array(
+      'activity_target_from_activity'
+    );
+  }
+}
