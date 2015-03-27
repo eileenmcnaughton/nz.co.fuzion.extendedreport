968bde4c35c244264e759901b19fcd092bb4cf33
Eileen McNaughton <eileen@fuzion.co.nz>
2015-03-27 13:45:35 +1300

    Initial commit on contribution pivot

---
+++ CRM/Extendedreport/Form/Report/Contribute/ContributionPivot.mgd.php
@@ -0,0 +1,19 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array(
+  0 => array(
+    'name' => 'Extended Report - Contribution Pivot Chart',
+    'entity' => 'ReportTemplate',
+    'params' => array(
+      'version' => 3,
+      'label' => 'Extended Report - Contribution Pivot Chart',
+      'description' => 'Extended Report - Contribution Pivot Chart',
+      'class_name' => 'CRM_Extendedreport_Form_Report_Contribute_ContributionPivot',
+      'report_url' => 'contribution/pivot',
+      'component' => 'CiviContribute',
+    ),
+  ),
+);
---
+++ CRM/Extendedreport/Form/Report/Contribute/ContributionPivot.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Class CRM_Extendedreport_Form_Report_ContributionPivot
+ */
+class CRM_Extendedreport_Form_Report_Contribute_ContributionPivot extends CRM_Extendedreport_Form_Report_ExtendedReport {
+  protected $_baseTable = 'civicrm_contribution';
+  protected $skipACL = FALSE;
+  protected $_customGroupAggregates = TRUE;
+  protected $_aggregatesIncludeNULL = TRUE;
+  protected $_aggregatesAddTotal = TRUE;
+  protected $_rollup = 'WITH ROLLUP';
+  public $_drilldownReport = array();
+  protected $_potentialCriteria = array();
+
+  /**
+   * Class constructor.
+   */
+  public function __construct() {
+    $this->_customGroupExtended['civicrm_contribution'] = array(
+      'extends' => array('Contribution'),
+      'filters' => TRUE,
+      'title' => ts('Contribution'),
+    );
+
+    $this->_columns = $this->getColumns('Contribution', array(
+        'fields' => FALSE,
+      )
+    );
+
+    $this->_aggregateRowFields = array(
+      'contribution_civireport:financial_type_id' => 'Financial Type',
+    );
+    $this->_aggregateColumnHeaderFields = array();
+    parent::__construct();
+  }
+
+  /**
+   * Get sql FROM clauses.
+   *
+   * @return array
+   */
+  public function fromClauses() {
+    return array();
+  }
+}
