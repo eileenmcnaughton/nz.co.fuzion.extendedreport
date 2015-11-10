fb5ba0d47b1c4fea651fcec5760497bf204c5b56
eileen <emcnaughton@wikimedia.org>
2015-11-09 20:46:17 -0800

    Add address fields to priceset report

--- CRM/Extendedreport/Form/Report/Price/Contributionbased.php
+++ CRM/Extendedreport/Form/Report/Price/Contributionbased.php
@@ -16,7 +16,8 @@ class CRM_Extendedreport_Form_Report_Price_Contributionbased extends CRM_Extende
       + $this->getColumns('Contribution')
       + $this->getColumns('PriceField')
       + $this->getColumns('PriceFieldValue')
-      + $this->getColumns('LineItem');
+      + $this->getColumns('LineItem')
+      + $this->getColumns('Address');
     parent::__construct();
   }
 
@@ -28,7 +29,8 @@ class CRM_Extendedreport_Form_Report_Price_Contributionbased extends CRM_Extende
       'lineItem_from_contribution',
       'contact_from_contribution',
       'priceFieldValue_from_lineItem',
-      'priceField_from_lineItem'
+      'priceField_from_lineItem',
+      'address_from_contact',
     );
   }
 }
