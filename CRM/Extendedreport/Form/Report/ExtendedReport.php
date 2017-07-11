bd1d3d1740006e50fc07958701a9f9ce7fbf4b07
Jitendra Purohit <jitendra@fuzion.co.nz>
2017-07-11 19:20:50 +0530

    Fix group_concat with rollup to concat all the values in the last row

--- CRM/Extendedreport/Form/Report/ExtendedReport.php
+++ CRM/Extendedreport/Form/Report/ExtendedReport.php
@@ -2992,6 +2992,16 @@ LEFT JOIN civicrm_contact {$field['alias']} ON {$field['alias']}.id = {$this->_a
       $this->assignSubTotalLines($rows);
     }
 
+    if (!empty($this->_rollup)) {
+      $lastIndex = key(array_slice($rows, -1, 1, TRUE));
+      //Do not concat non-stat field in the last row.
+      foreach ($rows[$lastIndex] as $key => &$val) {
+        if (!in_array($key, $this->_statFields)) {
+          $val = NULL;
+        }
+      }
+    }
+
     if (empty($rows)) {
       return;
     }
