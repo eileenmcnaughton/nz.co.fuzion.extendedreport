f2bf09c24802dde61c27f9f4bdb25121ae3090e4
eileen <emcnaughton@wikimedia.org>
2018-08-31 23:16:39 +1200

    enotice fix

--- CRM/Extendedreport/Form/Report/Pledge/Lybuns.php
+++ CRM/Extendedreport/Form/Report/Pledge/Lybuns.php
@@ -67,6 +67,11 @@ class CRM_Extendedreport_Form_Report_Pledge_Lybuns extends CRM_Extendedreport_Fo
       'type'    => CRM_Utils_Type::T_INT,
       'options' => $optionYear,
       'default' => date('Y'),
+      'is_filters' => TRUE,
+      'is_join_filters' => TRUE,
+      'is_fields' => FALSE,
+      'is_group_bys' => FALSE,
+      'is_order_bys' => FALSE,
     ];
     $this->_groupFilter = TRUE;
     $this->_tagFilter = TRUE;
