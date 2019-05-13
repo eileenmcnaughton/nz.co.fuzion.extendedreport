882605421708b181e54eb17877b565878e0d6b02
Pengyi Zhang <49378827+agileware-pengyi@users.noreply.github.com>
2019-05-13 17:48:11 +1000

    Adding Primary Email column to report 'Extended Report - Price Set Line Items report' (#265)
    * CIVICRM-1199 Add email column to price set line items report
    
    * CIVICRM-1199 new branch
    
    * CIVICRM-1199 using existing function and delete new one
    
    * Formatting

--- CRM/Extendedreport/Form/Report/Price/Lineitem.php
+++ CRM/Extendedreport/Form/Report/Price/Lineitem.php
@@ -45,6 +45,11 @@ class CRM_Extendedreport_Form_Report_Price_Lineitem extends CRM_Extendedreport_F
   public function __construct() {
     $this->_columns
       = $this->getColumns('Contact', array('order_by' => TRUE))
+      + $this->getColumns('Email', [
+          'fields' => TRUE,
+          'order_by' => FALSE,
+        ]
+      )
       + $this->getColumns('Event')
       + $this->getColumns('Participant')
       + $this->getColumns('Contribution', array('order_by' => TRUE))
@@ -81,6 +86,7 @@ class CRM_Extendedreport_Form_Report_Price_Lineitem extends CRM_Extendedreport_F
       'event_from_participant',
       'address_from_contact',
       'address_from_contribution',
+	    'email_from_contact',
     );
 
   }
