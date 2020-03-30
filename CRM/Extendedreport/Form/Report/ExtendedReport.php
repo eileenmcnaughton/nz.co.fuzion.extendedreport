5869599712b9367801c81433bd2bf11c3b9d0205
yashodha <yashodha@cividesk.com>
2020-03-30 10:45:30 +0530

    expose end date as filter for membership extended report

--- CRM/Extendedreport/Form/Report/ExtendedReport.php
+++ CRM/Extendedreport/Form/Report/ExtendedReport.php
@@ -3825,6 +3825,7 @@ WHERE cg.extends IN ('" . implode("','", $extends) . "') AND
             'title' => ts($options['prefix_label'] . ' Membership Cycle End Date'),
             'include_null' => TRUE,
             'is_group_bys' => TRUE,
+            'is_filters' => TRUE,
             'type' => CRM_Utils_Type::T_DATE,
             'operatorType' => CRM_Report_Form::OP_DATE,
           ],
