d223e1e4e4565e1a720d88f3759817965fd8fa67
eileen <emcnaughton@wikimedia.org>
2020-07-12 11:29:58 +1200

    Move join conditionality to the join function
    This is part of https://github.com/eileenmcnaughton/nz.co.fuzion.extendedreport/pull/357 but I'm not sure about the table
    alias change in that

--- CRM/Extendedreport/Form/Report/Event/ParticipantExtended.php
+++ CRM/Extendedreport/Form/Report/Event/ParticipantExtended.php
@@ -128,10 +128,8 @@ class CRM_Extendedreport_Form_Report_Event_ParticipantExtended extends CRM_Exten
       'phone_from_contact',
       'address_from_contact',
       'email_from_contact',
+      'related_contact_from_participant',
     ];
-    if ($this->isTableSelected('related_civicrm_contact')) {
-      $fromClauses[] = 'related_contact_from_participant';
-    }
     return $fromClauses;
   }
 
