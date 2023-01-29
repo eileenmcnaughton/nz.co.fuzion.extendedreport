22b801f952ceb0f972cd5646a85f858232d87aa2
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-01-29 19:50:38 +1300

    Update wording to refer to the real entity
    The whole concept of a ReportTemplate entity has mercifully not
    been ported to apiv4 & referring to it is confusing, even
    as we transition

--- CRM/Extendedreport/Form/Report/Contact/Extendedcontact.mgd.php
+++ CRM/Extendedreport/Form/Report/Contact/Extendedcontact.mgd.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * // This file declares a managed database record of type "ReportTemplate".
+ * // This file declares a managed database record of type "OptionValue".
  * The record will be automatically inserted, updated, or deleted from the
  * database as appropriate. For more details, see "hook_civicrm_managed" at:
  * http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
